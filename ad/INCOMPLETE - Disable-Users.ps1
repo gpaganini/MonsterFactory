9585699947d8968b5727ee1aba6d0565ead94806
Giovani Paganini <giovanipaganini@outlook.com>
2020-04-01 17:48:40 -0300

    Created AD Disable from CSV Script

--- ad/INCOMPLETE - Disable-Users.ps1
+++
@@ -1,8 +0,0 @@
-﻿$csv = "C:\temp\teste.csv"
-$arquivo = Import-Csv $csv
-
-$arquivo.foreach({
-    $oldName = Get-ADUser -Identity $_.user -Properties DisplayName | select DisplayName
-    Set-ADUser -Identity $_.user -DisplayName ("LAYOFF - " + $oldName.DisplayName)
-    Disable-ADAccount -Identity $_.user
-})
