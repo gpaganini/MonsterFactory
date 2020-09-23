ea7bee24306bf5f912ac7e2e8c8a3fda13bb3c9b
Giovani Paganini <giovanipaganini@outlook.com>
2020-09-23 13:49:54 -0300

    move disabled users to OU

---
+++ ad/scriptengine-iam/Update Usuarios/moveDisabled.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module ActiveDirectory
+$csv = ".\disabled-move.csv"
+$arquivo = Import-Csv -Path $csv -Delimiter ";"
+
+foreach ($User in $arquivo) {
+    Disable-ADAccount -Identity $User.Usuario
+    Move-ADObject -Identity $user.DN -TargetPath "OU=Contas Desabilitadas,OU=Automacao - Autoseg,DC=aviva,DC=com,DC=br"
+}
