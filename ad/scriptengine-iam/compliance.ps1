8d8f5c250e1a86051d172e0f66779e4a0dcc48d2
Giovani Paganini <giovanipaganini@outlook.com>
2021-01-22 10:14:58 -0300

    Create compliance.ps1

---
+++ ad/scriptengine-iam/compliance.ps1
@@ -0,0 +1,15 @@
+﻿Import-Module ActiveDirectory
+
+$adUsers = Get-ADUser -Filter * -Properties *
+
+foreach($user in $adUsers) {
+    if(($user.Office -notlike "Rio Quente" -and $user.Office -notlike "Costa do Sauipe") -or ($user.ipPhone -eq $null)) {
+        echo "$($user.Name) - $($user.Office)"
+    }
+}
+
+foreach ($user in $adUsers) {
+    if ($user.ipPhone -eq $null) {
+        echo $user.Name $user.ipPhone
+    }
+}
