7e07b11f4d765acab1ab86ad72b803c1d40393fd
Giovani Paganini <giovanipaganini@outlook.com>
2024-08-08 11:02:24 -0300

    created sql scripts for oracle and mssql

---
+++ sql/oracle-statistics.sql
@@ -0,0 +1,11 @@
+SELECT table_name,owner,object_type,last_analyzed,stale_stats
+FROM dba_tab_statistics
+WHERE stale_stats = 'NO'
+AND OWNER NOT IN ('ANONYMOUS', 'APEX_040200', 'APEX_PUBLIC_USER', 'APPQOSSYS', 'AUDSYS', 'BI', 'CTXSYS', 'DBSNMP', 'DIP',
+			'DVF', 'DVSYS', 'EXFSYS', 'FLOWS_FILES', 'GSMADMIN_INTERNAL', 'GSMCATUSER', 'GSMUSER', 'HR', 'IX', 
+            'LBACSYS', 'MDDATA', 'MDSYS', 'OE', 'ORACLE_OCM', 'ORDDATA', 'ORDPLUGINS', 'ORDSYS', 'OUTLN', 'PM', 
+            'SCOTT', 'SH', 'SI_INFORMTN_SCHEMA', 'SPATIAL_CSW_ADMIN_USR', 'SPATIAL_WFS_ADMIN_USR', 'SYS', 'SYSBACKUP', 
+            'SYSDG', 'SYSKM', 'SYSTEM', 'WMSYS', 'XDB', 'SYSMAN', 'RMAN', 'RMAN_BACKUP', 'OWBSYS', 'OWBSYS_AUDIT', 
+            'APEX_030200', 'MGMT_VIEW', 'OJVMSYS', 'XS$NULL', 'OLAPSYS', 'SECURITY', 'PERFSTAT')
+AND LAST_ANALYZED <= (sysdate  - 7)
+AND table_name NOT LIKE 'MLOG$_%'
